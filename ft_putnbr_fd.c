33e3ec36ac0977406094ac7ce72a764b9161239e
Arsene Rurangiza <arurangi@shi-r6-p5.s19.be>
2022-10-11 09:45:53 +0200

    completed putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 09:32:11 by arurangi          #+#    #+#             */
+/*   Updated: 2022/10/11 09:42:21 by arurangi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_putnbr_fd(int nb, int fd)
+{
+		char	ch;
+
+	if (nb > 9)
+	{
+		ft_putnbr_fd((nb / 10), fd);
+		ft_putnbr_fd((nb % 10), fd);
+	}
+	else if (nb == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+		ft_putnbr_fd(nb, fd);
+	}
+	else if (nb >= 0 && nb < 10)
+	{
+		ch = nb + '0';
+		ft_putchar_fd(ch, fd);
+	}
+}
+
+
+// int main(void)
+// {
+// 	ft_putnbr(677);
+// }
