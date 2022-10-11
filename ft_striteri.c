2cbc927af5912f147b603fc7a9b06a003f43577a
Arsene Rurangiza <arurangi@shi-r6-p5.s19.be>
2022-10-11 09:58:31 +0200

    completed striteri

---
+++ ft_striteri.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 09:51:41 by arurangi          #+#    #+#             */
+/*   Updated: 2022/10/11 09:57:22 by arurangi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// s: The string on which to iterate
+// f: The function to apply to each character
+
+/*
+Applies the function ’f’ on each character of
+the string passed as argument, passing its index
+as first argument. Each character is passed by
+address to ’f’ to be modified if necessary.
+*/
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+	
+	i = 0;
+	while(s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
