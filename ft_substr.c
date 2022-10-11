b1508e5de5106d5ad9d973c09943729e7224485b
Arsene Rurangiza <arurangi@shi-r6-p5.s19.be>
2022-10-11 09:46:49 +0200

    added substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:08:24 by arurangi          #+#    #+#             */
-/*   Updated: 2022/10/07 17:28:18 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/10/11 09:12:36 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,18 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 	ptr = malloc(sizeof(char) * len + 1);
 	if (!ptr)
 		return (NULL);
-	
+	if (start >= ft_strlen(s))
+	{
+		ptr[0] = '\0';
+		return (ptr);
+	}
 	i = 0;
-	while (((char *)s)[start + i] && i < len)
+	while (i < len && ((char *)s)[start + i])
 	{
 		ptr[i] = s[start + i];
 		i++;
 	}
+	ptr[i] = '\0';
 	return (ptr);
 }
 
